02b3f9d146e1c8914fc34d8bcae10ed9bc02aaa5
Jean-Marie Saurel <saurel@ipgp.fr>
2019-01-31 19:19:16 +0000

    First commit of MC3 statistics superproc

---
+++ CODE/matlab/seismic_moment.m
@@ -0,0 +1,32 @@
+function y=seismic_moment(x,law)
+%SEISMIC_MOMENT magnitude to seismic moment in N.m conversion equations.
+%	SEISMIC_MOMENT(MAGNITUDE) returns seismic moment in N.m for magnitude
+%   according to Kanamori law
+%
+%	SEISMIC_MOMENT(MAGNITUDE,LAW) specifies the law for magnitude to moment 
+%	calculation, using following available LAW:
+%
+%            kanamori : Kanamori et al. [1978]
+%         feuillard80 : Feuillard [1980]
+%
+%	SEISMIC_MOMENT returns float numbers. Use FLOOR or ROUND to produce integers.
+%
+%
+%   Author: J.M. Saurel, WEBOBS/IPGP
+%   Created: 2019-01-36, Paris, France
+%   Updated:
+
+if nargin < 2
+    law = 'kanamori';
+end
+
+switch lower(law)
+	case 'kanamori'
+        y = 10.^(9.1+1.5.*x);
+	case 'feuillard80'
+        y = 10.^(6.7+2.14.*x-0.054.*x.^2);
+	otherwise
+		error('unknown law %s.',law);
+end
+
+
