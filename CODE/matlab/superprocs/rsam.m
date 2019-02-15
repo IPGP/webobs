c27889607eb2cf4adf4a6dce2662d6d7a544f723
François Beauducel <beauducel@gmail.com>
2019-02-15 17:39:32 +0700

    new try to solve the underscore issue...

--- CODE/matlab/superprocs/rsam.m
+++ CODE/matlab/superprocs/rsam.m
@@ -190,7 +190,7 @@ for n = 1:length(N)
 		tlabel(tlim,P.GTABLE(r).TZ)
 
 		% title, status and additional information
-		P.GTABLE(r).GTITLE = varsub(pernode_title,V);
+		P.GTABLE(r).GTITLE = varsub(pernode_title,V,'tex');
 		P.GTABLE(r).GSTATUS = [tlim(2),D(n).G(r).last,D(n).G(r).samp];
 		P.GTABLE(r).INFOS = {''};
 		if ~isempty(k)
@@ -233,7 +233,7 @@ if isfield(P,'SUMMARYLIST')
 		if any(isnan(tlim))
 			tlim = minmax(cat(1,D.tfirstlast));
 		end
-		P.GTABLE(r).GTITLE = varsub(summary_title,V);
+		P.GTABLE(r).GTITLE = varsub(summary_title,V,'tex');
 		P.GTABLE(r).GSTATUS = [tlim(2),rmean(cat(1,G.last)),rmean(cat(1,G.samp))];
 		P.GTABLE(r).INFOS = {''};
 
@@ -343,7 +343,7 @@ if isfield(P,'SUMMARYLIST')
 			if any(isnan(tlim))
 				tlim = minmax(cat(1,D.tfirstlast));
 			end
-			P.GTABLE(r).GTITLE = varsub(sourcemap_title,V);
+			P.GTABLE(r).GTITLE = varsub(sourcemap_title,V,'tex');
 			P.GTABLE(r).GSTATUS = [tlim(2),rmean(cat(1,G.last)),rmean(cat(1,G.samp))];
 			P.GTABLE(r).INFOS = {''};
 
