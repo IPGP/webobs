457f63cce9b8fa0c63e1562060f00dc45d5e0eca
beaudu <beauducel@gmail.com>
2019-06-12 13:28:44 +0700

    adds possible cell string of valid values in field2str.m

--- CODE/matlab/field2str.m
+++ CODE/matlab/field2str.m
@@ -4,14 +4,17 @@ function s = field2str(x,f,s0,varargin)
 %	return the string content.
 %
 %	FIELD2STR(X,FIELD,S0) returns S0 string if the convertion fails (default
-%	is empty).
+%	return value is empty).
 %
 %	FIELD2STR(X,FIELD,S0,'notempty') returns also S0 if X.(FIELD) is empty.
+%
+%	FIELD2STR(X,FIELD,{S0,S1,...}) accepts only the values in cell {S0,S1,...}
+%	and returns default S0 if not.
 %
 %
 %	Author: F. Beauducel, WEBOBS/IPGP
 %	Created: 2016-01-29 at BPPTKG, Yogyakarta Indonesia
-%	Updated: 2016-07-10
+%	Updated: 2019-05-22
 
 if nargin < 2 && ~isstruct(x) & ~ischar(f)
 	error('Arguments X must be a structure and FIELD a string.')
@@ -26,9 +29,13 @@ if isfield(x,f) && (~isempty(x.(f)) || ~notempty)
 		s = num2str(x.(f));
 	end
 else
-	if nargin > 2 || (isfield(x,f) && isempty(x.(f)) && notempty)
+	if nargin > 2 && ischar(s0) || (isfield(x,f) && isempty(x.(f)) && notempty)
 		s = s0;
 	else
 		s = [];
 	end
+end
+
+if nargin > 2 && iscell(s0) && ~isempty(s0) && (isempty(s) || ~ismember(s,s0))
+	s = s0{1};
 end
