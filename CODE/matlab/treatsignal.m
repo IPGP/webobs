beecca6515e2e53e24e3c207af6e6907df01833f
beaudu <beauducel@gmail.com>
2019-06-22 14:51:56 +0700

    minor fixes to preprare next release

--- CODE/matlab/treatsignal.m
+++ CODE/matlab/treatsignal.m
@@ -3,14 +3,15 @@ function [tc,dc] = treatsignal(t,d,r,OPT)
 % 	[TC,DC]=TREATSIGNAL(T,D,R,OPT) processes vector signal D(T) in the following order:
 % 	   1. if OPT.FLAT_IS_NAN is OK, removes flat signals
 % 	   2. if OPT.PICKS_CLEAN_PERCENT exists and >0, removes picks above the value %
-% 	   3. if OPT.MEDIAN_FILTER_SAMPLES exists and >0, filters the signal using value samples 
-% 	   4. decimates by a factor of R (integer) using moving average,
-% 	   5. if OPT.UNDERSAMPLING is OK, decimation is undersampling instead of averaging
+% 	   3. if OPT.PICKS_CLEAN_STD exists and >0, removes picks above value*STD around mean
+% 	   4. if OPT.MEDIAN_FILTER_SAMPLES exists and >0, filters the signal using value samples 
+% 	   5. decimates by a factor of R (integer) using moving average,
+% 	   6. if OPT.UNDERSAMPLING is OK, decimation is undersampling instead of averaging
 %
 %
 %	Author: F. Beauducel / WEBOBS
 % 	Created: 2015-08-24
-% 	Updated: 2018-01-23
+% 	Updated: 2019-05-20
 
 
 % removes flat intervals
@@ -20,9 +21,11 @@ if isok(OPT,'FLAT_IS_NAN')
 		d(k+1) = NaN;
 	end
 end
-d = cleanpicks(d,field2num(OPT,'PICKS_CLEAN_PERCENT',0));
 
-% optional median filter
+% cleanpicks filters
+d = cleanpicks(d,OPT);
+
+% median filter
 m = field2num(OPT,'MEDIAN_FILTER_SAMPLES');
 if m > 1
 	d = mmed(d,m);
