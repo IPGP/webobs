9287b2b8311839b9b3bc598d14209ebec3623707
beaudu <beauducel@gmail.com>
2019-12-09 15:50:28 +0400

    add optional PCA in smartplot.m

--- CODE/matlab/smartplot.m
+++ CODE/matlab/smartplot.m
@@ -1,4 +1,4 @@
-function lre = smartplot(X,tlim,G,linestyle,fontsize,chnames,choffset,ndalias,ndcolor,ndtrend,timezoom,trendmindays)
+function [lre,V] = smartplot(X,tlim,G,linestyle,fontsize,chnames,choffset,timezoom,trendmindays)
 %SMARTPLOT Enhanced multi-component timeseries plot
 %	SMARTPLOT plots a single graph divided into channel subplots, each
 %	subplot grouping all nodes with different colors. This supposes all
@@ -19,6 +19,9 @@ function lre = smartplot(X,tlim,G,linestyle,fontsize,chnames,choffset,ndalias,nd
 %	   d: data matrix with several 'channels' d(:,i) with the same unit
 %	   e: error matrix (optional, same size, same unit as d)
 %	   w: data weight vector (0 = plain, 1 = 50% lighter, 2 = 67%, ...)
+%	 trd: trend flag (true plots a linear trend for corresponding node)
+%	 rgb: RGB color
+%	 nam: node's name string
 %
 %
 %	tlim: 2-element vector to define time period.
@@ -34,30 +37,36 @@ function lre = smartplot(X,tlim,G,linestyle,fontsize,chnames,choffset,ndalias,nd
 %
 %	choffset: scalar defining the space between each channel subplots
 %
-%	ndalias: cell of strings defining node names.
-%
-%	ndcolor: vector of index for scolor function.
-%
-%	ndtrend: vector of bolean (true = plots a linear trend for corresponding node).
-%
-%	timezoom: scalar defining the ratio of zoom.
+%	zoompca: scalar defining the ratio of zoom if positive or PCA if negative.
 %
 %	trendmindays: minimum time interval between 2 samples to compute a trend.
 %
 %
 %	Author: F. Beauducel / WEBOBS
 %	Created: 2019-05-14
-%	Updated: 2019-08-12
+%	Updated: 2019-10-29
 
 
 zoom = double(isinto(timezoom,[0,1],'exclude'));
-for ii = 0:zoom
+npca = (timezoom<0)*ceil(abs(timezoom));
+V = [];
+for ii = 0:(zoom+(timezoom<0))
+	% computes PCA
+	if npca > 0 && ii > 0
+		[y,V,D] = pca(X(npca).d);
+		if ~isempty(y)
+			X = struct('t',X(npca).t,'d',y,'w',X(npca).w,'e',X(npca).e*V,'nam',X(npca).nam,'rgb',X(npca).rgb,'trd',X(npca).trd);
+		else
+			X = struct('t',X(npca).t,'d',y,'w',X(npca).w,'e',X(npca).e,'nam',X(npca).nam,'rgb',X(npca).rgb,'trd',X(npca).trd);
+		end
+		chnames = split(strtrim(sprintf('Eig.#%d %1.1f%%|',[1:size(X.d,2);100*D'/sum(D(:))])),'|');
+	end
 	% computes global min/max for each component (all stations)
 	% completes missing columns
 	alld = cat(1,X.d);
 	% number of channels
 	nx = size(alld,2);
-	if ii > 0
+	if zoom && ii > 0
 		tlim = [-timezoom*diff(tlim),0] + tlim(2);
 		alld = alld(isinto(cat(1,X.t),tlim),:);
 	end
@@ -78,9 +87,11 @@ for ii = 0:zoom
 	end
 
 	% plots the data
-	subplot(4,1,ii*2 + (1:(4/(1+zoom)))), extaxes(gca,[.08,.04])
+	subplot(4,1,ii*2 + (1:(4/(1+zoom+(timezoom<0))))), extaxes(gca,[.08,.04])
 	hold on
-	lre = nan(nx,2);	
+	if ii == 0
+		lre = nan(nx,2);	
+	end
 	for i = 1:nx
 		if i < nx
 			fill([tlim,fliplr(tlim)],cmpoffset(i) + gmin(i) - [.25,.25,.75,.75]*choffset,'w','FaceColor',.95*ones(1,3),'EdgeColor','none')
@@ -91,10 +102,13 @@ for ii = 0:zoom
 				if isfield(X(n),'e') && ~isempty(X(n).e)
 					d = [d,X(n).e(:,i)];
 				end
-				plotorbit(X(n).t,d,X(n).w,linestyle,G.LINEWIDTH,G.MARKERSIZE,scolor(ndcolor(n)));
+				plotorbit(X(n).t,d,X(n).w,linestyle,G.LINEWIDTH,G.MARKERSIZE,X(n).rgb);
+				if npca > 0 && ii > 0
+					plotorbit(X(n).t,mavr(d(:,1),10),X(n).w,'-',G.LINEWIDTH,G.MARKERSIZE/2,scolor(3));
+				end 
 				kk = find(~isnan(d(:,1)));
-				if ndtrend(n) && length(kk) >= 2 && diff(minmax(X(n).t(kk))) >= trendmindays
-					if size(d,1) > 1 && all(d(kk,2)~=0)
+				if ii == 0 && X(n).trd && length(kk) >= 2 && diff(minmax(X(n).t(kk))) >= trendmindays
+					if size(d,2) > 1 && all(d(kk,2)~=0)
 						lr = wls(X(n).t(kk)-tlim(1),d(kk,1),1./d(kk,2).^2);
 					else
 						lr = wls(X(n).t(kk)-tlim(1),d(kk,1),ones(size(d(kk,1))));
@@ -144,13 +158,18 @@ for ii = 0:zoom
 
 	hold off
 
-	% node aliases (not empty elements only)
-	ka = find(~cellfun(@isempty,ndalias));
-	nl = length(ka);
-	fs = fontsize*max(min(50/length(strjoin(ndalias)),1),0.5);
-	for n = 1:nl
-		text(tlim(1)+n*diff(tlim)/(nl+1),ylim(2),ndalias(ka(n)),'Color',scolor(ndcolor(ka(n))), ...
-			'HorizontalAlignment','center','VerticalAlignment','bottom','FontSize',fs,'FontWeight','bold')
+	if ii==0
+		% node aliases (not empty elements only)
+		ka = find(~cellfun(@isempty,{X.nam}));
+		nl = length(ka);
+		fs = fontsize*max(min(50/length(strjoin({X.nam})),1),0.5);
+		for n = 1:nl
+			text(tlim(1)+n*diff(tlim)/(nl+1),ylim(2),X(ka(n)).nam,'Color',X(ka(n)).rgb, ...
+				'HorizontalAlignment','center','VerticalAlignment','bottom','FontSize',fs,'FontWeight','bold')
+		end
+	end
+	if npca > 0 && ii > 0
+		title('Principal Component Analysis: raw data and 10-day filtering','FontWeight','bold')
 	end
 	set(gca,'YLim',ylim);
 
