fc941d2e60fea78db29002fe0ec2eb981e1109b9
beaudu <beauducel@gmail.com>
2020-04-14 16:33:34 +0200

    improves mc3 data format and mc3stats filtering behavior

--- CODE/matlab/isinto.m
+++ CODE/matlab/isinto.m
@@ -1,6 +1,6 @@
 function k=isinto(x,lim,opt)
 %ISINTO Is into an interval
-%	ISINTO(X,[XMIN,XMAX]) returns a bolean vector of the same size as X, 
+%	ISINTO(X,[XMIN,XMAX]) returns a bolean vector of the same size as X,
 %	containing 1 where X is into the interval defined by [XLIM,XMAX]
 %	inclusively, and 0 elsewhere.
 %
@@ -14,8 +14,11 @@ function k=isinto(x,lim,opt)
 %
 %	Author: F. Beauducel / WEBOBS
 %	Created: 2014-12-17
-%	Updated: 2019-05-19
+%	Updated: 2020-04-14
 
+if isempty(lim)
+	lim = NaN;
+end
 if nargin < 3
 	opt = '';
 end
