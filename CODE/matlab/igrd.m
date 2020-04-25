ff21a9ecc6c64b6f5d2c27617024c26807049272
beaudu <beauducel@gmail.com>
2020-04-25 14:18:14 +0200

    improves user's DEM reading with igrid.m

--- CODE/matlab/igrd.m
+++ CODE/matlab/igrd.m
@@ -1,43 +1,43 @@
 function [x,y,z] = igrd(fn)
 %IGRD	Import DEM in .GRD formats (Surfer, ArcInfo / GMT).
-%	[X,Y,Z] = IGRD(FILENAME) returns the Digital Elevation Model data 
+%	[X,Y,Z] = IGRD(FILENAME) returns the Digital Elevation Model data
 %	defined by X and Y (vectors or matrix) and matrix Z of elevations.
 %	FILENAME is in one the data grid .GRD formats: Golden Sofware Surfer (ASCII
 %	or binary), Arc/Info (ASCII), or Generic Mapping Tool (ASCII only).
 %
 %	NoData values are replaced by NaN.
 %
-%	Author: François Beauducel, IPG Paris.
+%	Author: FranÃ§ois Beauducel, IPG Paris.
 %	Created: 1996
-%	Updated: 2019-10-16
+%	Updated: 2020-04-22
 %
 %	References:
 %	   Golden Software Surfer, http://www.goldensoftware.com/
 %	   GMT (Generic Mapping Tools), http://gmt.soest.hawaii.edu
 
-%	Copyright (c) 1996-2018, François Beauducel, covered by BSD License.
+%	Copyright (c) 1996-2020, FranÃ§ois Beauducel, covered by BSD License.
 %	All rights reserved.
 %
-%	Redistribution and use in source and binary forms, with or without 
-%	modification, are permitted provided that the following conditions are 
+%	Redistribution and use in source and binary forms, with or without
+%	modification, are permitted provided that the following conditions are
 %	met:
 %
-%	   * Redistributions of source code must retain the above copyright 
+%	   * Redistributions of source code must retain the above copyright
 %	     notice, this list of conditions and the following disclaimer.
-%	   * Redistributions in binary form must reproduce the above copyright 
-%	     notice, this list of conditions and the following disclaimer in 
+%	   * Redistributions in binary form must reproduce the above copyright
+%	     notice, this list of conditions and the following disclaimer in
 %	     the documentation and/or other materials provided with the distribution
-%	                           
-%	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-%	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-%	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-%	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-%	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-%	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-%	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-%	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-%	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-%	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+%
+%	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %	POSSIBILITY OF SUCH DAMAGE.
 
 % lauch Open File dialog box if no file is specified
@@ -56,15 +56,17 @@ fid = fopen(fn, 'r');
 line = fgets(fid);             % reads 1st line header
 
 % case of Golden Software/Surfer grid ASCII file
-if strfind(line,'DSAA')
+if regexpi(line,'^DSAA')
 	sz = fscanf(fid, '%d', [1 2]);
 	xm = fscanf(fid, '%f', 2);
 	ym = fscanf(fid, '%f', 2);
 	zm = fscanf(fid, '%f', 2);
+    fprintf('--> found GS/Surfer ascii grid: %dx%d, xlim = [%g %g], ylim = [%g %g], zlim = [%g %g] ...',sz,xm,ym,zm);
 	z = fscanf(fid, '%f', sz)';
+    fprintf(' done.\n');
 
 % case of Golden Software/Surfer grid BINARY file
-elseif strfind(line,'DSBB')
+elseif regexpi(line,'^DSBB')
 	fclose(fid);
 	fid = fopen(fn,'rb');
 	co = fscanf(fid,'%c',4);
@@ -72,12 +74,14 @@ elseif strfind(line,'DSBB')
 	xm = fread(fid,2,'float64')';
 	ym = fread(fid,2,'float64')';
 	zm = fread(fid,2,'float64')';
+    fprintf('--> found GS/Surfer binary grid: %dx%d, xlim = [%g %g], ylim = [%g %g], zlim = [%g %g] ...',sz,xm,ym,zm);
 	z = reshape(fread(fid,'float32'),sz)';
+    fprintf(' done.\n');
 
 % case of ESRI/ArcInfo grid ASCII file (added in 2009)
-elseif strfind(line,'ncols')
+elseif regexpi(line,'^ncols')
 	s = textscan(line,'%s');
-	
+
 	% the two formats exist: 'ncols xx' or 'xx ncols'...
 	if strcmpi(s{1}(1),'ncols')
 		x = 1;
@@ -121,9 +125,11 @@ elseif strfind(line,'ncols')
 	end
 	xm(2) = xm(1) + (sz(1)-1)*csx;
 	ym(2) = ym(1) + (sz(2)-1)*csy;	% note the reverse order of Y-axis vector
+    fprintf('--> found ESRI/AcrInfo ascii grid: %dx%d, xlim = [%g %g], ylim = [%g %g] ...',sz,xm,ym);
 	z = flipud(fscanf(fid, '%f', sz)');	% transform needed because Z values are sorted rowwise
-    
-elseif strfind(l,'CDF')
+    fprintf(' done.\n');
+
+elseif regexpi(line,'^CDF')
 	error('"%s" is a binary netCDF grid file. Cannot be read, yet...\nConvert it first using "grd2xyz -Ef" command.',fn)
 else
 	error('"%s" is not a valid GRD file for this function.',fn)
@@ -136,4 +142,3 @@ y = linspace(ym(1),ym(2),sz(2))';
 
 % replace NoData values by NaN
 z(z == ndv | abs(z) > 1e38) = NaN;
-
