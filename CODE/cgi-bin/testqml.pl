baff2d11390d00d1d16d0c89752c21ae3aac5d4f
Xavier Béguin <beguin@ipgp.fr>
2020-07-09 14:58:42 -0400

    Remove leftover test scripts from cgi-bin

--- CODE/cgi-bin/testqml.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-use QML;
-
-my %QML = qmlorigin('/home/www/webobs-datatest/continu/sismo/sc3_events/2012/04/25/ovpf2012idst/ovpf2012idst.last.xml');
-
-foreach(keys(%QML)) {
-	print "$_ = $QML{$_}\n";
-}
