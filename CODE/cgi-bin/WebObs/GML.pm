284dc8112c054d440d794c283955e626641cf283
Pierre S <sakic@ipgp.fr>
2022-12-07 18:09:40 +0100

    new version of GeodesyML reader, beta version

--- CODE/cgi-bin/WebObs/GML.pm
+++ CODE/cgi-bin/WebObs/GML.pm
@@ -11,8 +11,7 @@ use strict;
 use WebObs::XML2;
 
 #--------------------------------------------------------------------------------------------------------------------------------------
-# qmlvalues: returns origin and magmitude preferred values from XML2 arrayd
-sub gmlorigin {
+sub gmlread {
 	my $file = $_[0];
 	my %gml;
 	my @xml2;
@@ -31,45 +30,42 @@ sub gmlorigin {
 		my @IdsRec = findvalues("$root_rec/\@gml:id=",\@xml2);
 		my $id_rec = @IdsRec[-1];
 		$id_rec =~ s/^\s+|\s+$//g ; # very important, id must be trimmed
+		#print $id_rec;
 		## Get Last receiver Node
-		my @Rec = findnode($root_rec,"/\@gml:id=$id_rec",\@xml2);
+		my @Rec = findnodes($root_rec,"/\@gml:id=",$id_rec,\@xml2);
+		#print @Rec;
 		## Get useful values
-		$gml{rec_model}   = findvalue('/geo:igsModelCode=',\@Rec);
-		$gml{rec_serialn} = findvalue('/geo:manufacturerSerialNumber=',\@Rec);
-		$gml{rec_vfirm}   = findvalue('/geo:firmwareVersion=',\@Rec);
-		$gml{rec_satsys}  = findvalue('/geo:satelliteSystem=',\@Rec);
+		$gml{rec_model}  = findvalue('/geo:igsModelCode=',\@Rec);
+		$gml{rec_sn}     = findvalue('/geo:manufacturerSerialNumber=',\@Rec);
+		$gml{rec_vfirm}  = findvalue('/geo:firmwareVersion=',\@Rec);
+		$gml{rec_satsys} = findvalue('/geo:satelliteSystem=',\@Rec);
 		
 		###### Antenna
 		## root path for the Antenna
 		my $root_ant = "$root/geo:gnssAntenna/geo:GnssAntenna";
 		## XML id of the last Antenna
-		my @IdsRec = findvalues("$root_rec/\@gml:id=",\@xml2);
-		my $id_rec = @IdsRec[-1];
-		$id_rec =~ s/^\s+|\s+$//g ; # very important, id must be trimmed
-		## Get Last receiver Node
-		my @Rec = findnode($root_rec,"/\@gml:id=$id_rec",\@xml2);
+		my @IdsAnt = findvalues("$root_ant/\@gml:id=",\@xml2);
+		my $id_ant = @IdsAnt[-1];
+		$id_ant =~ s/^\s+|\s+$//g ; # very important, id must be trimmed
+		## Get Last Antenna Node
+		my @Ant = findnodes($root_ant,"/\@gml:id=",$id_ant,\@xml2);
+		#print @Ant,@IdsAnt;
 		## Get useful values
-		$gml{rec_model}   = findvalue('/geo:igsModelCode=',\@Rec);
-		$gml{rec_serialn} = findvalue('/geo:manufacturerSerialNumber=',\@Rec);
-		$gml{rec_vfirm}   = findvalue('/geo:firmwareVersion=',\@Rec);
-		$gml{rec_satsys}  = findvalue('/geo:satelliteSystem=',\@Rec);
+		$gml{ant_model}  = findvalue('/geo:igsModelCode=',\@Ant);
+		$gml{ant_sn}     = findvalue('/geo:manufacturerSerialNumber=',\@Ant);
 		
 		
-		#print @Rec;
-
+		####### Misc Info
 		my $domes = findvalue("$root/geo:siteIdentification/geo:iersDOMESNumber",\@xml2);
 
-		print $gml{rec_model};
-		
-		#my @Rec   = findvalues("$root_rec",\@xml2);
-		#print $#Rec +1;
+		print %gml;
 	}
 
-	return @xml2;
+	return %gml;
 	
 }
 
 my $p="/home/sakic/Downloads/ILAM00MTQ.xml";
-gmlorigin($p);
+gmlread($p);
 
 1;
