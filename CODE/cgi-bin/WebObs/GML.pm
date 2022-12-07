43a45b775c74e194ae6cdb05ca770a9aa3da2856
Pierre S <sakic@ipgp.fr>
2022-12-07 15:04:44 +0100

    working minimal content for rec

--- CODE/cgi-bin/WebObs/GML.pm
+++ CODE/cgi-bin/WebObs/GML.pm
@@ -19,24 +19,47 @@ sub gmlorigin {
 	
 	if (-e $file) {
 #		my @xml2 = qx($WEBOBS{XML2_PRGM} < $file);
-
 		my @xml2 = qx(/usr/bin/xml2 < $file);
+		
+		## common root path
 		my $root = '/geo:GeodesyML/geo:siteLog';
+		
+		###### Receiver
+		## root path for the Receiver
 		my $root_rec = "$root/geo:gnssReceiver/geo:GnssReceiver";
-		my $domes = findvalue("$root/geo:siteIdentification/geo:iersDOMESNumber",\@xml2);
-		
-		
-		my @IdsRec = findvalues('/geo:GeodesyML/geo:siteLog/geo:gnssReceiver/geo:GnssReceiver/@gml:id=',\@xml2);
+		## XML id of the last Receiver
+		my @IdsRec = findvalues("$root_rec/\@gml:id=",\@xml2);
 		my $id_rec = @IdsRec[-1];
-		my @Rec = findnode('/geo:GeodesyML/geo:siteLog/geo:gnssReceiver/geo:GnssReceiver',"/\@gml:id=gnss-receiver-6011908e8ef7b",\@xml2);
+		$id_rec =~ s/^\s+|\s+$//g ; # very important, id must be trimmed
+		## Get Last receiver Node
+		my @Rec = findnode($root_rec,"/\@gml:id=$id_rec",\@xml2);
+		## Get useful values
+		$gml{rec_model}   = findvalue('/geo:igsModelCode=',\@Rec);
+		$gml{rec_serialn} = findvalue('/geo:manufacturerSerialNumber=',\@Rec);
+		$gml{rec_vfirm}   = findvalue('/geo:firmwareVersion=',\@Rec);
+		$gml{rec_satsys}  = findvalue('/geo:satelliteSystem=',\@Rec);
 		
-		my $rec_satsys = findvalue('/geo:satelliteSystem=',\@Rec);
-		my $rec_satsys = findvalue('/geo:satelliteSystem=',\@Rec);
+		###### Antenna
+		## root path for the Antenna
+		my $root_ant = "$root/geo:gnssAntenna/geo:GnssAntenna";
+		## XML id of the last Antenna
+		my @IdsRec = findvalues("$root_rec/\@gml:id=",\@xml2);
+		my $id_rec = @IdsRec[-1];
+		$id_rec =~ s/^\s+|\s+$//g ; # very important, id must be trimmed
+		## Get Last receiver Node
+		my @Rec = findnode($root_rec,"/\@gml:id=$id_rec",\@xml2);
+		## Get useful values
+		$gml{rec_model}   = findvalue('/geo:igsModelCode=',\@Rec);
+		$gml{rec_serialn} = findvalue('/geo:manufacturerSerialNumber=',\@Rec);
+		$gml{rec_vfirm}   = findvalue('/geo:firmwareVersion=',\@Rec);
+		$gml{rec_satsys}  = findvalue('/geo:satelliteSystem=',\@Rec);
 		
-		print $id_rec;		
-		print $satsys;
-		print $id_rec;
-		print @Rec;
+		
+		#print @Rec;
+
+		my $domes = findvalue("$root/geo:siteIdentification/geo:iersDOMESNumber",\@xml2);
+
+		print $gml{rec_model};
 		
 		#my @Rec   = findvalues("$root_rec",\@xml2);
 		#print $#Rec +1;
