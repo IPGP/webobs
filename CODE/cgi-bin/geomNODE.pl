df0bdee42f51d0aa242905c3340ce3b50ff8ea2f
ldassin <l.dassin@orange.fr>
2023-03-22 09:43:05 +0100

    gridsMgr bientot fini et scripts complémentaires

--- CODE/cgi-bin/geomNODE.pl
+++ CODE/cgi-bin/geomNODE.pl
@@ -17,7 +17,7 @@ print $cgi->header( -type => 'text/plain', -status => '200' );
 
 # ---- extracting some data from the table producer
 my $driver   = "SQLite";
-my $database = "/opt/CONF/WEBOBSMETA.db";
+my $database = "/opt/webobs/CONF/WEBOBSMETA.db";
 my $dsn = "DBI:$driver:dbname=$database";
 my $userid = "";
 my $password = "";
