f898538f2ff07b3b5b34f7a458dabd9bbf4676c5
ldassin <l.dassin@orange.fr>
2023-05-26 09:47:45 +0200

    Add a link to Theia thesaurus through formCLB for the Theia Category

---
+++ CODE/cgi-bin/showTHEIA.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+print "Content-type:text/html\r\n\r\n";
+print '<html>';
+print '<head>';
+print '<title>Hello Word - First CGI Program</title>';
+print '</head>';
+print '<body>';
+print '<h2>Hello Word! This is my first CGI program</h2>';
+print '</body>';
+print '</html>';
+
+1;
