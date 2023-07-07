cb88bd46086ce75eaddada82deffb1581b8f5213
ldassin <l.dassin@orange.fr>
2023-07-07 11:50:30 +0200

    Added the possibility to delete row in the META database through the Theia form.

--- CODE/cgi-bin/showTHEIA.pl
+++ CODE/cgi-bin/showTHEIA.pl
@@ -6,7 +6,16 @@ showTHEIA.pl
 
 =head1 SYNOPSIS
 
-http://..../showTHEIA.pl
+http://..../showTHEIA.pl?object=object[,id=Identifier,action=delete]
+
+ object=
+ producer, dataset or observation.
+ 
+ id=
+ Identifier of the above object.
+
+ delete=
+ if present delete the row.
 
 =head1 DESCRIPTION
 
@@ -39,6 +48,13 @@ use WebObs::i18n;
 # ---- display database-related text content
 #print $cgi->header( -type => 'text/plain', -status => '200' );
 
+# ---- What are we supposed to do ?: find it out in the query string
+#
+my $QryParm    = $cgi->Vars;    # used later; todo: replace cgi->param's below
+my $object	   = $cgi->param('object')    // '';
+my $identifier = $cgi->param('id')        // '';
+my $action     = $cgi->param('action')    // '';
+
 # ---- connecting to the database
 my $driver   = "SQLite";
 my $database = $WEBOBS{SQL_METADATA};
@@ -47,6 +63,18 @@ my $userid = "";
 my $password = "";
 my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 })
    or die $DBI::errstr;
+   
+# ---- if action=delete, the field with the id=Identifier will be erased from the database ----------------------------------------------------
+
+if ($action eq "delete") {
+	my $stmt = qq(DELETE FROM $object WHERE identifier = \"$identifier\");
+	my $sth = $dbh->prepare( $stmt );
+	my $rv = $sth->execute() or die $DBI::errstr;
+
+	if($rv < 0) {
+	   print $DBI::errstr;
+	}
+}
 
 #$dbh->disconnect();
 
@@ -104,7 +132,8 @@ while(my @row = $sth->fetchrow_array()) {
 	$contacts  = join(',',split(/_,/,$row[7]));
 	$funders   = join(',',split(/_,/,$row[8]));
 	$onlineRes = join(',',split(/_,/,$row[9]));
-	print "<TR><TD width=3% align=center><SMALL><A href=\"/cgi-bin/gridsMgr.pl\">$row[0]</A>"
+	print "<TR><TD width=3% align=center><SMALL><A href=\"/cgi-bin/gridsMgr.pl\">$row[0]</A>&nbsp&nbsp"
+			."<A href=\"/cgi-bin/showTHEIA.pl?object=producer&id=$row[0]&action=delete\"><IMG style=\"width:10px;height:10px;\"title=\"delete producer\" src=\"/icons/no.png\"></A>"
 			."<p><input type=\"hidden\" name=\"producerId\" value=\"$row[0]\"></input></p></SMALL></TD>"
 			."<TD width=4% align=center><SMALL>$row[1]"
 			."<p><input type=\"hidden\" name=\"name\" value=\"$row[1]\"></input></p></SMALL></TD>"
@@ -157,7 +186,8 @@ while(my @row = $sth->fetchrow_array()){
 	my $nodeId  = (split '\.', $row[0]) [1];
 	my $proc    = (split '_', (split '\.', $row[0]) [0]) [2];
 	my $subject = join(',', split(/_/,$row[3]));
-	print "<TR><TD width=15% align=center><SMALL><A href=\"/cgi-bin/formNODE.pl?node=PROC.$proc.$nodeId\">$row[0]</A></SMALL></TD>"
+	print "<TR><TD width=15% align=center><SMALL><A href=\"/cgi-bin/formNODE.pl?node=PROC.$proc.$nodeId\">$row[0]</A>&nbsp&nbsp"
+			."<A href=\"/cgi-bin/showTHEIA.pl?object=datasets&id=$row[0]&action=delete\"><IMG style=\"width:10px;height:10px;\"title=\"delete producer\" src=\"/icons/no.png\"></A></SMALL></TD>"
 			."<TD width=14% align=center><SMALL>$row[1]</SMALL></TD>"
 			."<TD width=14% align=center><SMALL>$row[2]</SMALL></TD>"
 			."<TD width=14% align=center><SMALL>$subject</SMALL></TD>"
@@ -199,7 +229,8 @@ while(my @row = $sth->fetchrow_array()){
 	my $nodeId  = (split '_', (split '\.', $row[0]) [1]) [0];
 	my $proc    = (split '_', (split '\.', $row[0]) [0]) [2];
 	my $subject = join(',', split(/_/,$row[3]));
-	print "<TR><TD width=12% align=center><SMALL><A href=\"/cgi-bin/formCLB.pl?node=PROC.$proc.$nodeId\">$row[0]</A></SMALL></TD>"
+	print "<TR><TD width=12% align=center><SMALL><A href=\"/cgi-bin/formCLB.pl?node=PROC.$proc.$nodeId\">$row[0]</A>&nbsp&nbsp"
+			."<A id=$row[0] class=\"observations\" onclick=\"deleteRow(this);\" href=\"#\"><IMG style=\"width:10px;height:10px;\"title=\"delete producer\" src=\"/icons/no.png\"></A></SMALL></TD>"
 			."<TD width=6% align=center><SMALL>$row[1]</SMALL></TD>"
 			."<TD width=6% align=center><SMALL>$row[2]</SMALL></TD>"
 			."<TD width=12% align=center><SMALL>$row[3]</SMALL></TD>"
@@ -255,6 +286,14 @@ print <<"FIN";
 	function createOBS(\$form) {
 		console.log(\$form)
 	}
+	
+	function deleteRow(element) {
+		if (confirm(\"Do you really want to delete \"+element.id+\" ?\")) {
+			element.href=\"/cgi-bin/showTHEIA.pl?object=\"+element.className+\"&id=\"+element.id+\"&action=delete\";
+		} else {
+			console.log(element.className);
+		}
+	}
 
 	var \$form = \$("#formTHEIA");
 	var data = getFormData(\$form);
@@ -273,3 +312,30 @@ FIN
 
 print "<p><input type=\"submit\" onclick=\"\" name=\"valider\" value=\"Valider\"></p>";
 print "</form>";
+
+__END__
+
+=pod
+
+=head1 AUTHOR(S)
+
+Lucas Dassin
+
+=head1 COPYRIGHT
+
+Webobs - 2012-2023 - Institut de Physique du Globe Paris
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
