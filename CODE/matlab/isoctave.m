ad5554d2d4db0fb133461329938059ba66abc4fd
François Beauducel <beauducel@gmail.com>
2023-07-28 10:32:06 +0200

    improves isoctave.m

--- CODE/matlab/isoctave.m
+++ CODE/matlab/isoctave.m
@@ -1,9 +1,8 @@
 function y=isoctave
 %isoctave returns 1 (true) if the environment is GNU Octave
 
-V = ver;
-if isstruct(V) && isfield(V,'Name')
-    y = strcmpi(V(1).Name,'Octave');
+if exist('OCTAVE_VERSION', 'builtin')
+    y = true;
 else
     y = false;
 end
