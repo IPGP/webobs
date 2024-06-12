bb036de777681dd6945f86c54f9ee25d6ebdb472
François Beauducel <beauducel@gmail.com>
2024-06-12 17:29:34 +0200

    superproc gnss can use possible FID list for relative vectors

--- CODE/matlab/ismemberlist.m
+++ CODE/matlab/ismemberlist.m
@@ -1,11 +1,15 @@
-function lia = ismemberlist(A,B)
-%ISMEMBERLIST True for set member in coma-separated list
+function [lia,locb] = ismemberlist(A,B)
+%ISMEMBERLIST True for set member in coma-separated list A
 %
 %   Author: F. Beauducel, WEBOBS/IPGP
 %   Created: 2014-07-15
-%   Updated: 2016-07-10
+%   Updated: 2024-05-31
 
 lia = false(size(A));
+locb = zeros(size(A));
 for n = 1:length(A)
-	lia(n) = any(ismember(split(A{n},','),B));
+	if any(ismember(split(A{n},','),B))
+		lia(n) = true;
+		locb(n) = n;
+	end
 end
