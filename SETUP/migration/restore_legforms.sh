0470d17bac88539f11a4beca6c33e7c20e3ee65e
François Beauducel <beauducel@gmail.com>
2025-01-31 10:36:52 +0100

    new demo FORM.SPRINGS + cleanup legacy forms in SETUP + admin tool to restore forms (dev only)

---
+++ SETUP/migration/restore_legforms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# needs admin rights to run
+
+WOCONF=$(readlink /etc/webobs.d)
+WO=$(cd $WOCONF/.. && pwd)
+echo "WebObs root dir is $WO"
+
+for f in $(ls -d CONF/LEGACY_FORMS/FORMS/*); do
+	form=$(basename $f)
+	rm -f CONF/GRIDS2NODES/FORM.$form.*
+	rm -rf CONF/FORMS/$form
+	mv $f CONF/FORMS/
+	mv -f CONF/LEGACY_FORMS/GRIDS2FORMS/PROC.*.$form CONF/GRIDS2FORMS/
+    mv -f DATA/BACKUP_LEGACY_FORMS/$form.DAT DATA/DB/
+	echo "--> legacy form $form restored. Downgrade WebObs to release 2.7 or previous."
+done
