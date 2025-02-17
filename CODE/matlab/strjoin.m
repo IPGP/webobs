4fc7c8a4cc096a714a73a003f887a39fad74cd2a
François Beauducel <beauducel@gmail.com>
2025-02-17 18:37:11 +0100

    commit just to save ongoing work on GNSS active fault correction

--- CODE/matlab/strjoin.m
+++ CODE/matlab/strjoin.m
@@ -1,13 +1,17 @@
 function s=strjoin(c,d)
 %STRJOIN Join strings
-%	STRJOIN(C,DELIMITER) joins cell elements in C using string D as delimiter.
+%	STRJOIN(C,DELIMITER) joins non-empty elements in cell C using string D 
+%   as delimiter.
 %	STRJOIN(C) concatenates elements without delimiter.
 %
 %	Author: F. Beauducel, IPGP
+%   Updated: 2025-02-17
 
 if nargin < 2
 	d = '';
 end
+
+c(cellfun(@isempty,c)) = []; % remove empty elements
 n = numel(c);
 ss = cell(2,n);
 ss(1,:) = reshape(c,1,n);
