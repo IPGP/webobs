2885082a2304b0640b6e8c58da7adb7ef492653e
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-08-11 15:56:57 +0200

    Remove Octave equivalent handling for uistack in plotorbit function

--- CODE/matlab/plotorbit.m
+++ CODE/matlab/plotorbit.m
@@ -21,12 +21,6 @@ if size(d,2) > 1
 	h = plot(repmat(t,[1,2])',(repmat(d(:,1),[1,2])+d(:,2)*[-1,1])','-','LineWidth',.1,'Color',.6*[1,1,1]);
 	if exist('uistack','file') == 2 % checks if uistack function exists, (only MATLAB, not octave compatible)
 		uistack(h,'bottom')
-	else % Octave equivalent
-		set(h, 'HandleVisibility', 'off');
-		axes_children = get(gca, 'Children');
-		other_children = setdiff(axes_children, h);
-		set(gca, 'Children', [other_children; h]);
-		set(h, 'HandleVisibility', 'on');
 	end
 end
 
