08a8b9a2c931cd20385b0108e2a264dccf679693
François Beauducel <beauducel@gmail.com>
2026-02-22 16:43:03 +0400

    fix gnss.m + new shadow option in smarttext.m

--- CODE/matlab/mavr.m
+++ CODE/matlab/mavr.m
@@ -1,30 +1,32 @@
 function y = mavr(x,n,p)
 %MAVR	Moving average filtering.
 %	MAVR(x,n) returns signal X filtered by a moving average	on N 
-%	continuous data.
+%	continuous data. The first N-1 values of X will be NaN.
 %
-%	MAVR(x,n,p) adds a phase (late) of p data (p >=0).
+%	MAVR(x,n,p) adds a phase (late) of p data (p >= 0).
 %
 %	MAVR(x,n,-1) eliminates phase distortion (non-causal). Uses FILTFILT 
 %	funtion (Signal Processing Toolbox).
 %
-%	Author: F. Beauducel, IPGP/WEBOBS
+%	Author: F. Beauducel, IPGP
 %	Created: 1996
-%	Updated: 2019-02-16
+%	Updated: 2023-07-18
 
 if nargin < 2
 	error('MAVR requires at least 2 input arguments.')
 end
 
+m = size(x,1);
+
+if m == 1
+	x = x';
+end
+
+pp = 0;
 if nargin < 3
-	pp = 0;
 	p = 0;
 else 
-	if p < 0
-		pp = 0;
-	else
-		pp = p;
-	end
+	pp = p*(p < 0);
 end
 b = zeros(1,n + pp);
 b((pp+1):(n+pp)) = ones(1,n)/n;
@@ -37,11 +39,13 @@ for i = 1:size(x,2)
 		x0(i) = x(k,i);
 	end
 end
-xx0 = repmat(x0,[size(x,1) 1]);
 if p >= 0
-	y = filter(b,1,x - xx0);
+	y = filter(b,1,x - x0);
+    if length(y) > 0
+        y(1:min(length(y),n-1)) = NaN;
+    end
 else
-	y = filtfilt(b,1,x - xx0);
+	y = filtfilt(b,1,x - x0);
 end
-y = y + xx0;
+y = y + x0;
 
