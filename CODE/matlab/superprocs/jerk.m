1877c46b35a8c3b6375e7a846202da83c73ec224
François Beauducel <beauducel@gmail.com>
2026-03-26 16:03:39 +0400

    update comments on prediction stats (jerk.m)

--- CODE/matlab/superprocs/jerk.m
+++ CODE/matlab/superprocs/jerk.m
@@ -36,7 +36,7 @@ function DOUT=jerk(varargin)
 %
 %   Authors: F. Beauducel + G. Roult + V. Ferrazzini, WEBOBS/IPGP
 %   Created: 2014-04-14 at OVPF, La Réunion, Indian Ocean
-%   Updated: 2026-03-25
+%   Updated: 2026-03-26
 
 WO = readcfg;
 wofun = sprintf('WEBOBS{%s}',mfilename);
@@ -391,12 +391,12 @@ for n = 1:length(N)
 					%sprintf('   N-S tide: {\\bf none}'), ...
 				}];
 			end
-            % prediction based on past events stats
+            % statistics based on past events stats
             probe = 0;
             probi = 0;
             if jmax > threshold_level1
-                probe = 100*sum(jmax>=jmax_eruption)/nevt(1) % probability for an eruption
-                probi = 100*sum(jmax>=jmax_intrusion)/nevt(2); % probability for an intrusion
+                probe = 100*sum(jmax>=jmax_eruption)/nevt(1); % percentage of eruptions that occurred with this jmax (or below) 
+                probi = 100*sum(jmax>=jmax_intrusion)/nevt(2); % percentage of intrusions that occurred with this jmax (or below)
             end
 			OPT.INFOS = [OPT.INFOS{:},{ ...
 				sprintf('   Slope window: {\\bf %g s @%g s}',mw,dt), ...
@@ -404,7 +404,7 @@ for n = 1:length(N)
 				sprintf('Last JERK alert (in zoom window):'), ...
 				sprintf('   Max.: {\\bf %g nm/s%s}',roundsd(jmax,2),cb3), ...
 				sprintf('   Level1: %s',sal1),sprintf('   Level2: %s',sal2), ...
-				sprintf('JERK prediction probability:'), ...
+				sprintf('JERK prediction statistics:'), ...
                 sprintf('(based on {\\bf %g} past events)',sum(nevt)), ...
                 sprintf('   {\\bf %1.1f%%} Intrusion',probi), ...
                 sprintf('   {\\bf %1.1f%%} Eruption',probe), ...
@@ -501,7 +501,7 @@ for n = 1:length(N)
 					fprintf(fid,'\n\n');
                     fprintf(fid,'Maximum jerk amplitude = %g nm/%s\n\n',roundsd(jmax,2),cb3);
                     if nevt > 0
-                        fprintf(fid,'Jerk prediction (base %g past events):\n',nevt);
+                        fprintf(fid,'Jerk statistics (base %g past events):\n',nevt);
                         fprintf(fid,'  %1.1f%% intrusion\n',probi);
                         fprintf(fid,'  %1.1f%% eruption\n',probe);
                     end
