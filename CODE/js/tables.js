956e0a8bfc6fa3d3b18c64f14192a64030cedf13
François Beauducel <beauducel@gmail.com>
2026-09-24 18:55:07 +0400

    deal with empty cell, text and number for column sorting (tables.js)

--- CODE/js/tables.js
+++ CODE/js/tables.js
@@ -17,18 +17,18 @@ function sortTable(id, n) {
             shouldSwitch = false;
             /* Get the two elements you want to compare,
             one from current row and one from the next: */
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
+            x = cellValue(rows[i].getElementsByTagName("TD")[n]);
+            y = cellValue(rows[i + 1].getElementsByTagName("TD")[n]);
             /* Check if the two rows should switch place,
             based on the direction, asc or desc: */
             if (dir == "asc") {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                if (x > y) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
                 }
             } else if (dir == "desc") {
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                if (x < y) {
                     // If so, mark as a switch and break the loop:
                     shouldSwitch = true;
                     break;
@@ -50,5 +50,15 @@ function sortTable(id, n) {
                 switching = true;
             }
         }
+    }
+}
+/* if textContent is not empty, returns numerical value (if not null) or text,
+ otherwise returns innerHTML */
+function cellValue(cell) {
+    var v = cell.textContent.toLowerCase();
+    if (v != "") {
+        return isNaN(v) ? v : parseFloat(v);
+    } else {
+        return cell.innerHTML.toLowerCase();
     }
 }
